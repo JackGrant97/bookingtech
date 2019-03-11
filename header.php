7306743169566aff19b37f1ee3d0504f6fd60b7f
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-03-11 14:37:25 +0000

    Update header.php

--- header.php
+++ header.php
@@ -6,10 +6,16 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title></title>
   </head>
   <body>
-
+      <script type="text/javascript" src="js/materialize.min.js"></script>
       <header>
         <nav>
           <div class="nav-wrapper">
