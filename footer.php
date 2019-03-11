5e512fda4ae21527e23f7124f61c213289ca2522
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-03-11 23:22:04 +0000

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,9 @@
+<!DOCTYPE html>
 <html>
   <footer>
     <div class="container-fluid">
       <div class="foot-content bg-dark">
+        <button type="button" name="button"></button>
       </div>
     </div>
   </footer>
