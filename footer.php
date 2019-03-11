f7e8e04534a7aa3c3d6e3a8617bd36134f3755e7
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-03-11 23:45:54 +0000

    px

--- footer.php
+++ footer.php
@@ -3,7 +3,9 @@
   <footer class="fixed-bottom">
     <div class="container-fluid bg-dark">
       <div class="foot-content">
-        <p class="copyright">© Copyright 2019 - All Rights Reserved</p>
+        <p class="copyright">
+          © Copyright 2019 - All Rights Reserved
+        </p>
       </div>
     </div>
   </footer>
