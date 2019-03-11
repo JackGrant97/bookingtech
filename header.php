d1d47f7a0be97ad66d9eae522a85af7fcd0e5f92
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-03-11 22:42:23 +0000

    Update header.php

--- header.php
+++ header.php
@@ -10,6 +10,12 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <link type="text/css" rel="stylesheet" href="css/style.css">
     <title></title>
+    <script>
+      $('#myTab a').on('click', function (e) {
+      e.preventDefault()
+      $(this).tab('show')
+      })
+    </script>
   </head>
   <body>
       <header>
