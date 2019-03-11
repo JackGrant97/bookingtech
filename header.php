59b132d7697bbb9de4a91a0e3cbb6004d811ba61
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-03-11 21:47:56 +0000

    Update header.php

--- header.php
+++ header.php
@@ -36,7 +36,7 @@
                 <input type="password" name="pwd" placeholder="Password...">
                 <button type="submit" class="btn btn-light" name="login-submit">Login</button>
               </form>
-              <a class="btn btn-primary" href="signup.php"></a>
+              <a class="btn btn-light" href="signup.php">Register</a>
               <form action="includes/logout.inc.php" method="post">
                 <button type="submit" class="btn btn-light" name="logout-submit">Logout</button>
               </form>
