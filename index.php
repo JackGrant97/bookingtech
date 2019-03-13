e6ee4176028641548362e9bd89bee9f8de7ebeb2
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-03-13 10:47:21 +0000

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,13 @@
 ?>
 
   <main>
+    <div class="jumbotron">
+      <h1 class="display-4">Welcome!</h1>
+      <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+      <hr class="my-4">
+      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </div>
     <div class="videobg">
       <video autoplay muted loop id="myVideo">
        <source src="assets/videos/walk.mp4" type="video/mp4">
