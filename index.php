4cc44f78d14ccc0e2b4d74897d7ed321be19c8bf
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-03-13 10:25:35 +0000

    video bg

--- index.php
+++ index.php
@@ -3,7 +3,11 @@
 ?>
 
   <main>
-
+    <div class="videobg">
+      <video autoplay muted loop id="myVideo">
+       <source src="assets/videos/walk.mp4" type="video/mp4">
+      </video>
+    </div>
   </main>
 
 <?php
