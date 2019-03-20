26d3baf703413cac6d4f31785909671e057d0455
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-03-20 18:08:19 +0000

    Update register.php

--- register.php
+++ register.php
@@ -8,24 +8,24 @@
         <div class="row">
           <div class="form-group col-md-6">
             <label for="formGroupExampleInput">Username/Business name</label>
-            <input type="text" class="form-control" id="formGroupExampleInput" pattern="[A-Za-z0-9 ]+" placeholder="Username/Business name..." class="validate">
+            <input type="text" class="form-control" name="uname" id="uname" pattern="[A-Za-z0-9 ]+" placeholder="Username/Business name..." class="validate">
           </div>
           <div class="form-group col-md-6">
             <label for="formGroupExampleInput2">Email</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="E-mail..." class="validate">
+            <input type="text" class="form-control" name="email" id="email" placeholder="E-mail..." class="validate">
           </div>
         </div>
         <div class="row">
           <div class="form-group col-md-6">
             <label for="formGroupExampleInput2">Password</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" pattern="[A-Za-z0-9 ]+" placeholder="Password..." class="validate">
+            <input type="text" class="form-control" name="pwd" id="pwd" pattern="[A-Za-z0-9 ]+" placeholder="Password..." class="validate">
             <small id="passwordHelpBlock" class="form-text text-muted">
               Your password must only use upper/lowercase letters and/or numbers.
             </small>
           </div>
           <div class="form-group col-md-6">
             <label for="formGroupExampleInput2">Confirm Password</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" pattern="[A-Za-z0-9 ]+" placeholder="Confirm Password..." class="validate">
+            <input type="text" class="form-control" name="cpwd" id="cpwd" pattern="[A-Za-z0-9 ]+" placeholder="Confirm Password..." class="validate">
             <small id="passwordHelpBlock" class="form-text text-muted">
               Ensure password matches.
             </small>
@@ -34,7 +34,7 @@
         <div class="row">
           <div class="form-group col-md-12">
             <label for="formGroupExampleInput2">Mobile</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" pattern="[0-9]+" placeholder="Mobile/Telephone..." maxlength="11" class="validate">
+            <input type="text" class="form-control" name="mobile" id="mobile" pattern="[0-9]+" placeholder="Mobile/Telephone..." maxlength="11" class="validate">
           </div>
         </div>
         <button type="submit" class="btn btn-light" name="signup-submit">Submit</button>
