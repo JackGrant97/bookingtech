00e9ac3bd0918319f3ce6cb2105e14c7c5eb49dd
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-03-20 15:57:02 +0000

    Update register.php

--- register.php
+++ register.php
@@ -6,20 +6,22 @@
     <div class="container">
       <form class="register">
         <div class="row">
-          <div class="form-group">
+          <div class="form-group col-md-6">
             <label for="formGroupExampleInput">Username</label>
             <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input">
           </div>
-        </div>
-        <div class="row">
-          <div class="form-group">
+          <div class="form-group col-md-6">
             <label for="formGroupExampleInput2">Email</label>
             <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
           </div>
         </div>
         <div class="row">
-          <div class="form-group">
+          <div class="form-group col-md-6">
             <label for="formGroupExampleInput2">Password</label>
+            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+          </div>
+          <div class="form-group col-md-6">
+            <label for="formGroupExampleInput2">Confirm Password</label>
             <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
           </div>
         </div>
@@ -31,17 +33,11 @@
         </div>
         <div class="row">
           <div class="form-group">
-            <label for="formGroupExampleInput2">Confirm Password</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
-          </div>
-        </div>
-        <div class="row">
-          <div class="form-group">
             <label for="formGroupExampleInput2">Email</label>
             <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
           </div>
         </div>
-        <button type="submit" class="btn btn-light" name="login-submit">Login</button>
+        <button type="submit" class="btn btn-light" name="login-submit">Submit</button>
       </form>
     </div>
   </main>
