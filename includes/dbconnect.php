61a2634b32ec727ce453c18815490a84bd8af19d
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-03-20 15:35:36 +0000

    Update dbconnect.php

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 //connect to mysql database/ Host / username / password /
-$con = mysqli_connect("eu-cdbr-west-02.cleardb.net", "b7670d7664a5e4", ":c15655eb", "heroku_a20704f39159226") or die("Connection Failed" .
+$con = mysqli_connect("eu-cdbr-west-02.cleardb.net", "b7670d7664a5e4", "c15655eb", "heroku_a20704f39159226") or die("Connection Failed" .
 mysqli_error($con));
 
 
