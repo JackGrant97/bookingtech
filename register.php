c3f0bf9ade1f1bd0cf7cb3d07704472cb77d5c53
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-03-20 15:42:35 +0000

    row

---
+++ register.php
@@ -0,0 +1,48 @@
+<?php
+  require 'header.php';
+?>
+
+  <main>
+    <form>
+      <div class="row">
+        <div class="form-group">
+          <label for="formGroupExampleInput">Username</label>
+          <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input">
+        </div>
+      </div>
+      <div class="row">
+        <div class="form-group">
+          <label for="formGroupExampleInput2">Email</label>
+          <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+        </div>
+      </div>
+      <div class="row">
+        <div class="form-group">
+          <label for="formGroupExampleInput2">Password</label>
+          <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+        </div>
+      </div>
+      <div class="row">
+        <div class="form-group">
+          <label for="formGroupExampleInput2">Mobile</label>
+          <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+        </div>
+      </div>
+      <div class="row">
+        <div class="form-group">
+          <label for="formGroupExampleInput2">Confirm Password</label>
+          <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+        </div>
+      </div>
+      <div class="row">
+        <div class="form-group">
+          <label for="formGroupExampleInput2">Email</label>
+          <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+        </div>
+      </div>
+    </form>
+  </main>
+
+<?php
+  require 'footer.php';
+?>
