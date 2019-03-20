4debdbe12b006028842af3dce303efd5b4c71344
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-03-20 15:33:12 +0000

    con setup

---
+++ includes/dbconnect.php
@@ -0,0 +1,10 @@
+<?php
+//connect to mysql database/ Host / username / password /
+$con = mysqli_connect("eu-cdbr-west-02.cleardb.net", "b7670d7664a5e4", ":c15655eb", "heroku_a20704f39159226") or die("Connection Failed" .
+mysqli_error($con));
+
+
+?>
+
+
+<!--mysql://b7670d7664a5e4:c15655eb@eu-cdbr-west-02.cleardb.net/heroku_a20704f39159226?reconnect=true-->
