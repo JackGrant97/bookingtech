fcf4a8dc033909f84cda8684a6d1f15d03199605
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-03-20 13:34:04 +0000

    mvid

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
       <a class="btn btn-outline-light" href="#" role="button">Learn more</a>
     </div>
     <div class="videobg">
-      <video autoplay muted loop id="myVideo">
+      <video autoplay muted loop id="myVideo" class="myVideo">
        <source src="assets/videos/walk.mp4" type="video/mp4">
       </video>
     </div>
