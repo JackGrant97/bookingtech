64ebf1a095f823527460c9285ff51d681eea5280
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-03-20 16:21:34 +0000

    Update register.php

--- register.php
+++ register.php
@@ -8,27 +8,27 @@
         <div class="row">
           <div class="form-group col-md-6">
             <label for="formGroupExampleInput">Username</label>
-            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Username...">
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Username..." class="validate">
           </div>
           <div class="form-group col-md-6">
             <label for="formGroupExampleInput2">Email</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="E-mail...">
+            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="E-mail..." class="validate">
           </div>
         </div>
         <div class="row">
           <div class="form-group col-md-6">
             <label for="formGroupExampleInput2">Password</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Password...">
+            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Password..." class="validate">
           </div>
           <div class="form-group col-md-6">
             <label for="formGroupExampleInput2">Confirm Password</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Confirm Password...">
+            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Confirm Password..." class="validate">
           </div>
         </div>
         <div class="row">
           <div class="form-group col-md-12">
             <label for="formGroupExampleInput2">Mobile</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Mobile/Telephone...">
+            <input type="text" class="form-control" id="formGroupExampleInput2" pattern="[0-9]+" placeholder="Mobile/Telephone..." class="validate">
           </div>
         </div>
         <button type="submit" class="btn btn-light" name="login-submit">Submit</button>
