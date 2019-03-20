9b66fb9a339943616a58fca6508138b195981d0d
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-03-20 17:14:44 +0000

    Update booking.php

--- booking.php
+++ booking.php
@@ -5,16 +5,12 @@
   <main>
     <div class="container">
       <form class="form-group" method="post">
-        <div class="row">
-          <label class="my-1 mr-2" for="inlineFormCustomSelectPref">Select Table Size</label>
-        </div>
-        <div class="row">
+        <label class="my-1 mr-2" for="inlineFormCustomSelectPref">Select Table Size</label>
           <select class="custom-selec t my-1 mr-sm-2" id="inlineFormCustomSelectPref">
             <option selected>Choose...</option>
             <option value="6ft">6ft</option>
             <option value="12ft">12ft</option>
           </select>
-        </div>
         <button type="submit" class="btn btn-primary my-1">Submit</button>
       </form>
     </div>
