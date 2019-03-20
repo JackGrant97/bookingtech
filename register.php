0def5ecd06ab5f042540c0066631ac9cf11cee45
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-03-20 16:26:41 +0000

    Update register.php

--- register.php
+++ register.php
@@ -8,7 +8,7 @@
         <div class="row">
           <div class="form-group col-md-6">
             <label for="formGroupExampleInput">Username</label>
-            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Username..." class="validate">
+            <input type="text" class="form-control" id="formGroupExampleInput" pattern="[A-Za-z0-9 ]+" placeholder="Username..." class="validate">
           </div>
           <div class="form-group col-md-6">
             <label for="formGroupExampleInput2">Email</label>
@@ -18,11 +18,11 @@
         <div class="row">
           <div class="form-group col-md-6">
             <label for="formGroupExampleInput2">Password</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Password..." class="validate">
+            <input type="text" class="form-control" id="formGroupExampleInput2" pattern="[A-Za-z0-9 ]+" placeholder="Password..." class="validate">
           </div>
           <div class="form-group col-md-6">
             <label for="formGroupExampleInput2">Confirm Password</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Confirm Password..." class="validate">
+            <input type="text" class="form-control" id="formGroupExampleInput2" pattern="[A-Za-z0-9 ]+" placeholder="Confirm Password..." class="validate">
           </div>
         </div>
         <div class="row">
