3e45e38f3add44423118db3d6c8701453e905272
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-03-21 17:39:47 +0000

    Update about.php

--- about.php
+++ about.php
@@ -13,13 +13,13 @@
           </ol>
           <div class="carousel-inner">
             <div class="carousel-item active">
-              <img class="d-block w-100" src="http://placehold.jp/900x600.png" alt="First slide">
+              <img class="d-block w-100" src="http://placehold.jp/900x500.png" alt="First slide">
             </div>
             <div class="carousel-item">
-              <img class="d-block w-100" src="http://placehold.jp/900x600.png" alt="Second slide">
+              <img class="d-block w-100" src="http://placehold.jp/900x500.png" alt="Second slide">
             </div>
             <div class="carousel-item">
-              <img class="d-block w-100" src="http://placehold.jp/900x600.png" alt="Third slide">
+              <img class="d-block w-100" src="http://placehold.jp/900x500.png" alt="Third slide">
             </div>
           </div>
           <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
