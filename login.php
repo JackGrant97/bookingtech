634e611b78c4e01443ac74fe6f06dfbcaacd96b9
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-03-21 16:24:46 +0000

    Update login.php

--- login.php
+++ login.php
@@ -6,7 +6,7 @@
     <div class="status">
       <?php
         if (isset($_SESSION['userid'])) {
-          echo '<p><b>Welcome Back</b></p>'. $_SESSION['FirstName'];
+          echo '<p><b>Welcome Back</b></p>'. $_SESSION['Username'];
         }
         else {
           echo '<p><b>You are Logged out!</b></p>';
