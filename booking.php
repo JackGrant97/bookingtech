f4b196204ae02a162b267812c1a77a7dc98722e3
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-03-21 19:44:43 +0000

    Update booking.php

--- booking.php
+++ booking.php
@@ -8,7 +8,7 @@
         <div class="row">
           <div class="form-group col-md-12">
             <label class="my-1 mr-2" for="inlineFormCustomSelectPref">Select Days</label>
-            <select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
+            <select class="custom-select my-1 mr-sm-2" id="date" name="date">
               <option selected>Choose...</option>
               <option value="27th">27th July 2019</option>
               <option value="28th">28th July 2019</option>
@@ -19,9 +19,9 @@
         <div class="row">
           <div class="form-group col-md-12">
             <label class="my-1 mr-2" for="inlineFormCustomSelectPref">Select Table Size</label>
-            <select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
+            <select class="custom-select my-1 mr-sm-2" id="table" name="table">
               <option selected>Choose...</option>
-              <option value="6ft">6Ft (£175 Per Day)</option>
+              <option idvalue="6ft">6Ft (£175 Per Day)</option>
               <option value="12ft">12ft (£250 Per Day)</option>
             </select>
           </div>
