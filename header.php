e580d422cd40ab8f5b09c82d3d7dc1e7504a3599
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-03-22 22:14:00 +0000

    nav

--- header.php
+++ header.php
@@ -19,48 +19,49 @@
   </head>
   <body>
       <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-          <a class="navbar-brand" href="index.php">BookingTech</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item">
-                <a class="nav-link" href="index.php">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="booking.php">Booking</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="about.php">About</a>
-              </li>
-            </ul>
-            <ul class="navbar-nav">
-              <div class="header-login">
-                <form action="includes/login.inc.php" method="post">
-                  <input class="input" type="text" name="email" placeholder="E-mail...">
-                  <input class="input" type="password" name="password" placeholder="Password...">
-                  <button type="submit" class="btn btn-light" name="login-submit">Login</button>
-                  <?php
-                    if (isset($_SESSION['userid'])) {
-                      echo
-                      '
-                      <a class="btn btn-light" href="includes/logout.inc.php">Logout</a>
-                      ';
-                    }
-                    else {
-                      echo
-                      '
-                        <a class="btn btn-light" href="includes/signup.inc.php">Register</a>
-                      ';
-                    }
-                   ?>
-                </form>
-              </div>
-
-            </ul>
-          </div>
-        </nav>
+        <div class="nav">
+          <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="index.php">BookingTech</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                  <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="booking.php">Booking</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="about.php">About</a>
+                </li>
+              </ul>
+              <ul class="navbar-nav">
+                <div class="header-login">
+                  <form action="includes/login.inc.php" method="post">
+                    <input class="input" type="text" name="email" placeholder="E-mail...">
+                    <input class="input" type="password" name="password" placeholder="Password...">
+                    <button type="submit" class="btn btn-light" name="login-submit">Login</button>
+                    <?php
+                      if (isset($_SESSION['userid'])) {
+                        echo
+                        '
+                        <a class="btn btn-light" href="includes/logout.inc.php">Logout</a>
+                        ';
+                      }
+                      else {
+                        echo
+                        '
+                          <a class="btn btn-light" href="includes/signup.inc.php">Register</a>
+                        ';
+                      }
+                     ?>
+                  </form>
+                </div>
+              </ul>
+            </div>
+          </nav>
+        </div>
       </header>
 </html>
