b495640016c7e17b9a75d91a0eff028ed97fe371
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-03-22 21:06:56 +0000

    fp

--- about.php
+++ about.php
@@ -65,6 +65,11 @@
             <div id="collapseOne" class="collapse collapsed" aria-labelledby="headingOne" data-parent="#accordion">
               <div class="card-body">
                 Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+
+                <br> A full floor plan is availble down bellow
+                <br>
+                <br>
+                <a href="assets/images/Floorplan.png" class="btn btn-light" download="Floorplan.png">Floor Plan</a>
               </div>
             </div>
           </div>
